e22b9f64bb7e4d218894a1941510b32fa16bc3bb
RootAba <Abassemaiga403@gmail.com>
2022-08-17 13:04:46 +0200

    commit 2

--- src/main/java/com/example/Apigestionregions/controller/RegionController.java
+++ src/main/java/com/example/Apigestionregions/controller/RegionController.java
@@ -1,7 +1,11 @@
 package com.example.Apigestionregions.controller;
 
 
+import com.example.Apigestionregions.modele.Pays;
+import com.example.Apigestionregions.modele.Population;
 import com.example.Apigestionregions.modele.Regions;
+import com.example.Apigestionregions.service.PaysServImplement;
+import com.example.Apigestionregions.service.PopServImplement;
 import com.example.Apigestionregions.service.RegionServImplement;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -17,19 +21,28 @@ import java.util.List;
 public class RegionController {
 @Autowired
 private RegionServImplement reSeImpt;
-
+    @Autowired
+    private PaysServImplement paysSeImpt;
+    @Autowired
+    private PopServImplement popServImplement;
     @ApiOperation(value = "Ajout de region en tenant compte du pays et de la population")
     @PostMapping("/creation")
-public Regions creation(@RequestBody Regions region){
-    return  reSeImpt.AjoutRegion(region);
+public Regions creation(@RequestBody Regions region, Population population){
+
+        Pays pays =region.getPays();
+      //  if(pays==null){
+            paysSeImpt.ajouterPays(region.getPays());
+            popServImplement.ajouterPopulation(population);
+     //   }
+        return  reSeImpt.AjoutRegion(region);
 }
 
     @ApiOperation(value = "Affichage de la liste des régions avec Pays")
     @GetMapping("/afficher")
 public List<Regions> afficher(){
-    return  reSeImpt.affichageRegions();
-}
 
+        return  reSeImpt.affichageRegions();
+}
     @ApiOperation(value = "Modification  d'une  region en tenant compte du pays et de la population")
 @PutMapping("/modifier/{idRegion}")
 public Regions modifer(@PathVariable Long idRegion,@RequestBody Regions regions){
